eb6ace692e3be8958d360cb5caf8f80fe7ae6447
Bojie Jiang <jiangbojie@gmail.com>
2017-02-05 22:26:33 -0800

    Added dependencies

--- app/src/main/java/us/bojie/dagger_mvp_rx_database_android/MainActivity.java
+++ app/src/main/java/us/bojie/dagger_mvp_rx_database_android/MainActivity.java
@@ -1,13 +1,17 @@
 package us.bojie.dagger_mvp_rx_database_android;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BasicActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected int getContentView() {
+        return 0;
     }
 }
---
+++ app/src/main/java/us/bojie/dagger_mvp_rx_database_android/application/CakeApplication.java
@@ -0,0 +1,20 @@
+package us.bojie.dagger_mvp_rx_database_android.application;
+
+import android.app.Application;
+
+/**
+ * Created by bojiejiang on 2/5/17.
+ */
+
+public class CakeApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+    }
+}
