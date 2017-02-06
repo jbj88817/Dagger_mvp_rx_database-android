eb5caedbf0f25a9c3e2ebcff38890431309b3301
Bojie Jiang <jiangbojie@gmail.com>
2017-02-05 22:17:59 -0800

    First commit and BaseActivity

---
+++ app/src/main/java/us/bojie/dagger_mvp_rx_database_android/BasicActivity.java
@@ -0,0 +1,47 @@
+package us.bojie.dagger_mvp_rx_database_android;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bojiejiang on 2/5/17.
+ */
+
+public abstract class BasicActivity extends AppCompatActivity {
+
+    private ProgressDialog mProgressDialog;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(getContentView());
+        onViewReady(savedInstanceState, getIntent());
+    }
+
+    protected abstract int getContentView();
+
+    protected void onViewReady(Bundle savedInstanceState, Intent intent) {
+        //To be used by child activities;
+    }
+
+    protected void showDialog(String message) {
+        if (mProgressDialog == null) {
+            mProgressDialog = new ProgressDialog(this);
+            mProgressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+            mProgressDialog.setCancelable(true);
+        }
+
+        mProgressDialog.setMessage(message);
+        mProgressDialog.show();
+    }
+
+    protected void hideDialog() {
+        if (mProgressDialog != null && mProgressDialog.isShowing()) {
+            mProgressDialog.dismiss();
+        }
+
+    }
+}
